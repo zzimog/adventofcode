0d43c69b3e85fde1d6592c6abcf12290a0978350
grasim <simone.graziano@zucchetti.it>
2024-12-13 10:52:23 +0100

    day 12

--- day12/Plant.js
+++ day12/Plant.js
@@ -29,6 +29,31 @@ export class Plant {
   }
 
   get perimeter() {
-    return 4 - this.adj;
+    return 4 - this.adj.length;
+  }
+
+  get corners() {
+    const mask = [
+      [0, 0, 0],
+      [0, 1, 0],
+      [0, 0, 0],
+    ];
+
+    let corners = 0;
+
+    this.adj.forEach(([dR, dC]) => (mask[1 + dR][1 + dC] = 1));
+    this.diag.forEach(([dR, dC]) => (mask[1 + dR][1 + dC] = 1));
+
+    if (mask[0][0] == 0 && mask[0][1] == mask[1][0]) corners++;
+    if (mask[0][2] == 0 && mask[0][1] == mask[1][2]) corners++;
+    if (mask[2][0] == 0 && mask[2][1] == mask[1][0]) corners++;
+    if (mask[2][2] == 0 && mask[2][1] == mask[1][2]) corners++;
+
+    if (mask[0][0] == 1 && mask[0][1] == 0 && mask[1][0] == 0) corners++;
+    if (mask[0][2] == 1 && mask[0][1] == 0 && mask[1][2] == 0) corners++;
+    if (mask[2][0] == 1 && mask[2][1] == 0 && mask[1][0] == 0) corners++;
+    if (mask[2][2] == 1 && mask[2][1] == 0 && mask[1][2] == 0) corners++;
+
+    return corners;
   }
 }
--- day12/index.js
+++ day12/index.js
@@ -2,7 +2,7 @@ import { join } from 'node:path';
 import { getFileLines } from '../utils.js';
 import getRegions from './getRegions.js';
 
-const input = join(import.meta.dirname, './test.txt');
+const input = join(import.meta.dirname, './input.txt');
 const garden = await getFileLines(input, (l) => l.split(''));
 const regions = getRegions(garden);
 
@@ -10,12 +10,12 @@ const regions = getRegions(garden);
  * Part 1
  */
 
-// expected for test.txt -> 1930
 const sum1 = regions.reduce((sum, region) => {
   const A = region.plants.length;
   const P = region.plants.reduce((p, plant) => {
     return (p += plant.perimeter);
   }, 0);
+
   return (sum += A * P);
 }, 0);
 
@@ -25,17 +25,13 @@ console.log('Part 1, sum: %d', sum1);
  * Part 2
  */
 
-// expected for test.txt -> 1206
-const sum2 = 0;
-regions[0].plants.forEach((plant) => {
-  let matrix = [
-    [0, 0, 0],
-    [0, 1, 0],
-    [0, 0, 0],
-  ];
+const sum2 = regions.reduce((sum, region) => {
+  const A = region.plants.length;
+  const P = region.plants.reduce((sum, plant) => {
+    return (sum += plant.corners);
+  }, 0);
 
-  plant.adj.forEach(([dR, dC]) => (matrix[1 + dR][1 + dC] = 1));
-  plant.diag.forEach(([dR, dC]) => (matrix[1 + dR][1 + dC] = 1));
+  return (sum += A * P);
+}, 0);
 
-  console.table(matrix);
-});
+console.log('Part 2, sum: %d', sum2);
