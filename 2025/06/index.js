25fd00f9197d61e35e14be8753d362cdf1c9cc29
grasim <simone.graziano@zucchetti.it>
2025-12-09 11:05:28 +0100

    2025/06

---
+++ 2025/06/index.js
@@ -0,0 +1,90 @@
+import { getFileLines } from '../../utils.js';
+
+const FILE_NAME = './input.txt';
+
+/**
+ * Part 1
+ */
+
+async function sum1() {
+  const lines = await getFileLines(FILE_NAME, (line) => {
+    return line.match(/[\d|*|+]+/g);
+  });
+
+  const values = lines.slice(0, -1);
+  const operators = lines[lines.length - 1];
+  const length = operators.length;
+
+  return Array.from({ length }).reduce((sum, _, i) => {
+    const op = operators[i];
+    let tmp = op === '+' ? 0 : 1;
+
+    for (const col of values) {
+      const value = parseInt(col[i]);
+
+      if (op === '+') {
+        tmp += value;
+      } else if (op === '*') {
+        tmp *= value;
+      }
+    }
+
+    return sum + tmp;
+  }, 0);
+}
+
+console.log(await sum1());
+
+/**
+ * Part 2
+ */
+
+async function sum2() {
+  const lines = await getFileLines(FILE_NAME, (line) => {
+    return line.split('').reverse().join('');
+  });
+
+  const operators = lines[lines.length - 1].replace(/\s*/g, '').split('');
+
+  const values = lines
+    .slice(0, -1)
+    .reverse()
+    .map((l) => l.split(''))
+    .reverse();
+
+  const newValues = [];
+  let group = [];
+  for (let i = 0; i < values[0].length; i++) {
+    let str = '';
+
+    for (let j = 0; j < values.length; j++) {
+      str += values[j][i];
+    }
+
+    if (str.trim() === '') {
+      newValues.push(group);
+      group = [];
+      continue;
+    }
+
+    group.push(parseInt(str));
+  }
+  newValues.push(group);
+
+  return newValues.reduce((sum, group, i) => {
+    const op = operators[i];
+    let tmp = op === '+' ? 0 : 1;
+
+    for (const val of group) {
+      if (op === '+') {
+        tmp += val;
+      } else if (op === '*') {
+        tmp *= val;
+      }
+    }
+
+    return sum + tmp;
+  }, 0);
+}
+
+console.log(await sum2());
